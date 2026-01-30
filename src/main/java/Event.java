8113f7e0463e1c626dbc4ffeb090a246e6da62f9
Alexander Gerald <alex.gg.handoko@gmail.com>
2026-01-30 10:35:01 +0800

    Save user entries on disk
    Dippy did not have persistent record of user entries.
    
    This did not allow user to close and open Dippy with
    memory of the previous entries.
    
    Let's
    * make space locally for users to save their tasks
    * save and load from this space to Dippy
    
    Using polymorphism to convert tasks into their
    respective save formats reduce code complexity.

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -8,10 +8,38 @@ public class Event extends Task {
         this.endDate = endDate;
     }
 
+    public String getStartDate() {
+        return this.startDate;
+    }
+
+    public String getEndDate() {
+        return this.endDate;
+    }
+
+    @Override
+    public String saveFormat() {
+        String taskType = "E";
+        String oneOrZero = super.getDone() ? "1" : "0";
+        return taskType + " | " + oneOrZero + " | " + super.getName() + " | "
+            + startDate + " | " + endDate;
+    }
+
+    public static Event parseToTask(String[] arr) {
+        boolean isTaskDone = arr[1].equals("1") ? true : false;
+        String taskName = arr[2];
+        String startDate = arr[3];
+        String endDate = arr[4];
+        Event newTask = new Event(taskName, startDate, endDate);
+        if (isTaskDone) {
+            newTask.markAsDone();
+        }
+        return newTask;
+    }
+
     @Override
     public String toString() {
         String out = "[E]";
-        if (getIsDone()) {
+        if (getDone()) {
             out += "[X] ";
         } else {
             out += "[ ] ";
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -11,12 +11,28 @@ public class Task {
         this.isDone = true;
     }
 
-    public boolean getIsDone() {
+    public boolean getDone() {
         return this.isDone;
     }
 
     public String getName() {
         return this.name;
+    }
+
+    public String saveFormat() {
+        String taskType = "T";
+        String oneOrZero = this.isDone ? "1" : "0";
+        return taskType + " | " + oneOrZero + " | " + this.name;
+    }
+
+    public static Task parseToTask(String[] arr) {
+        boolean isTaskDone = arr[1].equals("1") ? true : false;
+        String taskName = arr[2];
+        Task newTask = new Task(taskName);
+        if (isTaskDone) {
+            newTask.markAsDone();
+        }
+        return newTask;
     }
 
     @Override
